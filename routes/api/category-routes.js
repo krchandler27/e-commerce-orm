decce7fc0310d4e9444acf8ed14598c00f6e6cbe
krchandler27 <krchandler27@gmail.com>
2023-02-19 17:13:05 -0700

    category-routes complete. Working on product-routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,91 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
+ // find all categories
   // be sure to include its associated Products
-});
+router.get('/', async (req, res) => {
+   try {
+    const categoryData = await Category.findAll(
+      {
+        include: [{
+          model: Product,
+          required: true
+        }]
+      });
 
-router.get('/:id', (req, res) => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'There are currently no categories in the database.' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+}});
+
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  router.get('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.findByPk(req.params.id,
+        {
+          include: [{
+            model: Product,
+            required: true
+          }]
+        });
 
-router.post('/', (req, res) => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category exists with inputted ID!' });
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+  }});
+
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.json(newCategory)
+  } catch(err) {
+    res.status(500).json(err)
+  }
 });
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category exists with inputted ID!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+    try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category exists with inputted ID!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,20 +3,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
-
+router.get('/', async (req, res) => {
+try {
+  const productData = await Product.findAll(
+    {
+      include: [{
+        model: Category,
+        required: true
+      },
+    {
+      model: Tag,
+      required: true
+    }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'There are currently no products in the database.'})
+      return;
+    }
+res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }});
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
